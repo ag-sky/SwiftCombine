8abbee8c3679809a4db0d71bc9be82400345b7e9
Akash-Gupta-yash <ag.office00@gmail.com>
2024-10-24 22:16:57 +0530

    Initial Commit

---
+++ CombineSwiftExample/CombineSwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSwiftExampleApp.swift
+//  CombineSwiftExample
+//
+//  Created by Deeksha Verma on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineSwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
